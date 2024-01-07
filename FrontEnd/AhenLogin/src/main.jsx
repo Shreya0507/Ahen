8a6a1d1f470ac9334eeeac0f9260981cb6542270
Shreya0507 <shreyawaghole0507@gmail.com>
2024-01-07 19:23:41 +0530

    new changes

---
+++ FrontEnd/AhenLogin/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Signup from './Components/Signup/Signup.jsx'
+import Signin from './Components/Signin/Signin.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+import Welcome from './Components/Welcome/Welcome.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='signin' element={<Signin />} />
+      <Route path='signup' element={<Signup />} />
+      <Route path='welcome' element={<Welcome />} />
+
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
